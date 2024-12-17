25a9d655008c14d0211d12cd3c6eb8cc9139f321
Ralph Woiwode <rawoiwode@gmail.com>
2024-12-16 20:06:00 -0600

    Working on the rough draft of the Resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,55 +2,48 @@ import Link from "next/link";
 
 const CV = () => {
   return (
-    <div className="mx-auto w-11/12 rounded-2xl border-8 border-emerald-950 border-opacity-75 bg-slate-50 p-4">
-      <h2 className="px-4 pt-4 font-bold text-red-800 underline underline-offset-8">
-        Summarized Career History
-      </h2>
+    <div className="mx-auto w-2/5 p-4">
       <ul className="list-inside py-4">
         <li className="mb-8 p-4">
           <section className="font-medium">
-            <h3 className="text-xl">General Motors</h3>
-            <div className="md:ml-4">
-              <h4>Data Engineer | January 2021 - April 2023</h4>
-              <h5>Austin, TX</h5>
-              <p className="pt-2">
-                My main project during this time was the{" "}
-                <b>Warranty Process Supplier Algorithm (WPSA)</b> app.
-                <br />
-                The entire Big Data team was migrating from Legacy architecture
-                (Hadoop/YARN) to a NextGen architecture(Kubernetes/ESS). I was
-                part of the complete rewrite for WPSA which helped optimizing
-                its functionality. We leveraged k8s, Docker, and Spark to
-                automate supplier cost forecasting, resulting in a 30% increase
-                in efficiency.
-              </p>
-              <p>Other tasks included:</p>
-              <ul className="list-inside list-disc">
-                <li>Applying queries to work with new table definitions</li>
-                <li>Fine tuning Spark configurations</li>
-                <li>Fine tuning Autosys job configurations</li>
-              </ul>
-              <h4 className="pt-4">
-                Software Developer, Front-end | August 2019 - December 2020
-              </h4>
-              <h5>Austin, TX</h5>
-              <p className="pt-2">
-                My primary project on this team was to rewrite the UI of
-                eCommerce&apos;s teams B2B application, transitioning from Java
-                to React. All of us on the team were new to React so it was
-                anexciting venture.
-                <br />
-                We conducted in-depth code reviews to identify programming
-                errors, and bugs. I integrated a Linter into the project to help
-                maintain consistent code styling. I also lead the adding of many
-                core React concepts (Reducer & Context) and showed the team how
-                to use them. We also conducted unit testing of Components using
-                Jest.
-                <br />
-                Overall, this rewrite resulted in a roughly 30% increase in user
-                involvement.
-              </p>
-            </div>
+            <h3 className="text-xl">Data Engineer @ General Motors</h3>
+            <h4>Jan 2021 - Apr 2023</h4>
+            <p className="pt-2">
+              My main project during this time was the{" "}
+              <u>Warranty Process Supplier Algorithm (WPSA)</u> app.
+              <br />
+              The entire Big Data team was migrating from Legacy architecture
+              (Hadoop/YARN) to a NextGen architecture(Kubernetes/ESS). I was
+              part of the complete rewrite for WPSA which helped optimizing its
+              functionality. We leveraged k8s, Docker, and Spark to automate
+              supplier cost forecasting, resulting in a 30% increase in
+              efficiency.
+            </p>
+            <p>Other tasks included:</p>
+            <ul className="list-inside list-disc">
+              <li>Applying queries to work with new table definitions</li>
+              <li>Fine tuning Spark configurations</li>
+              <li>Fine tuning Autosys job configurations</li>
+            </ul>
+            <h3 className="pt-4 text-xl">
+              Software Developer, Front-end @ General Motors
+            </h3>
+            <h4>AUG 2019 - DEC 2020</h4>
+            <p className="pt-2">
+              My primary project on this team was to rewrite the UI of
+              eCommerce&apos;s teams B2B application, transitioning from Java to
+              React. All of us on the team were new to React so it was
+              anexciting venture.
+              <br />
+              We conducted in-depth code reviews to identify programming errors,
+              and bugs. I integrated a Linter into the project to help maintain
+              consistent code styling. I also lead the adding of many core React
+              concepts (Reducer & Context) and showed the team how to use them.
+              We also conducted unit testing of Components using Jest.
+              <br />
+              Overall, this rewrite resulted in a roughly 30% increase in user
+              involvement.
+            </p>
           </section>
         </li>
         <hr className="m-8" />
