7ff41e0b31b692886edbd02fa402e2284f68e136
Ralph Woiwode <rawoiwode@gmail.com>
2024-12-28 20:07:26 -0600

    Laying out skeleton of what should be on the page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,30 +1,35 @@
 import Link from "next/link";
 
-const Current = () => {
+const Projects = () => {
   return (
-    <div className="mx-auto flex w-11/12">
-      <div className="mx-auto basis-1/4 rounded-2xl border-8 border-slate-500 border-opacity-50 bg-white p-4 outline outline-4 outline-white">
-        <h2 className="p-4 text-2xl font-bold text-emerald-500 underline underline-offset-8">
-          Current Work
-        </h2>
-        <ul>
+    <div className="mx-auto w-1/3 content-center">
+      <div className="mx-auto p-4">
+        <ul className="space-y-4">
           <li>
-            <Link href={"https://rawoiwode.com/"}>This</Link>
+            <div className="bg-background-800 p-6">
+              <Link
+                href={"https://rawoiwode.com/"}
+                className="text-accent-200 underline"
+              >
+                This
+              </Link>
+              <p>witty description</p>
+              <p>rough time estimate</p>
+            </div>
           </li>
           <li>
-            <Link href={"https://betoycaty.mx/"}>Wedding Site</Link>
+            <Link
+              href={"https://betoycaty.mx/"}
+              className="text-accent-200 underline"
+            >
+              Wedding Site
+            </Link>
           </li>
           <li>Appointment App (WIP)</li>
         </ul>
-      </div>
-      <div className="mx-auto basis-1/2 rounded-2xl border-8 border-slate-500 border-opacity-50 bg-white p-4 outline outline-4 outline-white">
-        <h2 className="p-4 text-2xl font-bold text-emerald-500 underline underline-offset-8">
-          Skills Breakdown
-        </h2>
-        <h4>List all relevant hard/soft skills</h4>
       </div>
     </div>
   );
 };
 
-export default Current;
+export default Projects;
