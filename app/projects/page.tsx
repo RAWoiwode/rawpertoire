076fbbbadc04440e8b1de901d349e2bbec88766e
Ralph Woiwode <rawoiwode@gmail.com>
2024-12-29 19:17:51 -0600

    Creating ProjectCard to display Project info neatly

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,32 +1,35 @@
 import Link from "next/link";
+import ProjectCard from "./components/ProjectCard";
 
 const Projects = () => {
   return (
     <div className="mx-auto w-1/3 content-center">
-      <div className="mx-auto p-4">
-        <ul className="space-y-4">
-          <li>
-            <div className="bg-background-800 p-6">
-              <Link
-                href={"https://rawoiwode.com/"}
-                className="text-accent-200 underline"
-              >
-                This
-              </Link>
-              <p>witty description</p>
-              <p>rough time estimate</p>
-            </div>
-          </li>
-          <li>
-            <Link
-              href={"https://betoycaty.mx/"}
-              className="text-accent-200 underline"
-            >
-              Wedding Site
-            </Link>
-          </li>
-          <li>Appointment App (WIP)</li>
-        </ul>
+      <div className="mx-auto space-y-8">
+        <ProjectCard>
+          <Link
+            href={"https://rawoiwode.com/"}
+            className="text-accent-200 underline"
+          >
+            This
+          </Link>
+          <p>witty description</p>
+          <p>rough time estimate</p>
+        </ProjectCard>
+        <ProjectCard>
+          <Link
+            href={"https://betoycaty.mx/"}
+            className="text-accent-200 underline"
+          >
+            Wedding Site
+          </Link>
+          <p>witty description</p>
+          <p>rough time estimate</p>
+        </ProjectCard>
+        <ProjectCard>
+          Appointment App (WIP)
+          <p>witty description</p>
+          <p>rough time estimate</p>
+        </ProjectCard>
       </div>
     </div>
   );
