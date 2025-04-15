6cb6ba4d84e5f5a23398e9858905bd7d53d9a141
Ralph Woiwode <rawoiwode@gmail.com>
2025-04-14 20:08:51 -0500

    Updating documentation for types

--- app/types/projectTypes.ts
+++ app/types/projectTypes.ts
@@ -1,11 +1,12 @@
 /**
- * Props for the Project component.
+ * Props for a single project.
+ *
  * @interface Project
  * @property {string} title - The title of the project.
  * @property {string} url - The URL of the project. If empty, the title will not be a link.
  * @property {string} description - A brief description of the project.
- * @property {string} image - Filename for the project's Identicon image.
- * @property {string} gitHubUrl - Filename for the project's Identicon image.
+ * @property {string} image - The filename for the project's Identicon image.
+ * @property {string} gitHubUrl - The GitHub repository URL for the project.
  */
 export interface Project {
   title: string;
